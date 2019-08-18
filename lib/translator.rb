7f76d7750159cbf6126d9b77114223ebe191858d
Andrew Zajac <ajzajac@icloud.com>
2019-08-18 13:46:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,38 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file)
+  return YAML.load_file(file)
+  library = YAML.load_file(file)
+  new_hash = {"get_meaning" =>{}, "get_emoticon" =>{}}
+  library.each do |key, value|
+    new_hash["get_meaning"][value[1]] = key
+    new_hash["get_emoticon"][value[0]] = value[1]
+  end
+ new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_japanese_emoticon(eng_emoji)
+  load_library.each do |key, value|
+    if eng_emoji == value[0]
+      return value[1]
+    end
+  end
+end
+
+def get_english_emoji(jap_emoji)
+  load_library.each do |key, val|
+    if jap_emoji == val[1]
+      return val[0]
+    end
+  end
+end
+  
+
+def get_japanese_emoticon(file, emoticon)
+  
+  
 end
 
 def get_english_meaning
