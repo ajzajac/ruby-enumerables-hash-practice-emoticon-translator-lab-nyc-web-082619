40c13c2053b618dc7e41df86457b8bb5b734009e
Andrew Zajac <ajzajac@icloud.com>
2019-08-18 13:58:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,7 +34,7 @@ def get_japanese_emoticon(file, emoticon)
   if library["get_emoticon"].include?(emoticon)
      library["get_emoticon"][emoticon]
  else
-   
+   "Sorry that emoticon was not found"
  
 end
 
